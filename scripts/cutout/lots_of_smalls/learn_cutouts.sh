5c30f07a7267ded62dc88f10fa844b1edaca3f65
Arkady Siuris <arss860@gmail.com>
2021-04-29 02:20:14 +0300

    -- removed

--- scripts/cutout/lots_of_smalls/learn_cutouts.sh
+++ scripts/cutout/lots_of_smalls/learn_cutouts.sh
@@ -1,6 +1,6 @@
 nohup python3.7 -u train.py --root "datasets/mask_cutout_100_10x10_gray_patch" --device "cuda:1" --lr "1e-4" --epochs 100 --save "cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer" --cutout --transfer "autoencoder/gaussian_noise/0_5_10epochs.pt" --checkpoint -1 > logs/cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer.log &
 
-nohup python3.7 -u train.py --root "datasets/mask_cutout_100_10x10_gray_patch_cut_mask" --device "cuda:1" --lr "1e-4" --epochs 100 --save "cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_cut_mask" --cutout --transfer "autoencoder/gaussian_noise/0_5_10epochs.pt" --checkpoint -1 -- > logs/cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_cut_mask.log &
+nohup python3.7 -u train.py --root "datasets/mask_cutout_100_10x10_gray_patch_cut_mask" --device "cuda:1" --lr "1e-4" --epochs 100 --save "cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_cut_mask" --cutout --transfer "autoencoder/gaussian_noise/0_5_10epochs.pt" --checkpoint -1 > logs/cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_cut_mask.log &
 
 nohup python3.7 -u train.py --root "datasets/mask_cutout_100_10x10_gray_patch_drop_initial" --device "cuda:2" --lr "1e-4" --epochs 100 --save "cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_drop_initial" --cutout --transfer "autoencoder/gaussian_noise/0_5_10epochs.pt" --checkpoint -1 > logs/cutouts/100epochs_1e-4_mask_cutout_100_10x10_gray_patch_transfer_drop_initial.log &
 
