8bcd679727fede1038b4857d8e23224fbb42c3c9
Arkady Siuris <arss860@gmail.com>
2021-05-11 17:12:27 +0300

    wandb.watch doesnt work for some reason

--- train_segmentation.py
+++ train_segmentation.py
@@ -61,8 +61,8 @@ def train_as_segmantation(model, data_loader, test_loader, mode='train', num_epo
     else:
         model.eval()
 
-    if not no_wandb:
-        wandb.watch(model, log_freq=100)
+    # if not no_wandb:
+    #     wandb.watch(model, log_freq=100)
 
     def criterion(x, mask):
         # x = torch.sigmoid(x)
